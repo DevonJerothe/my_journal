2b603aa1278a8f5f89b4a136be06d3fac35ab648
Devon Jerothe <devonjerothe@gmail.com>
2022-03-29 12:42:54 -0400

    update to MVVM
    - remove controllers
    - user repo's for obj control
    - fix AppLaunchView login check

--- lib/Repository/user_repo.dart
+++ lib/Repository/user_repo.dart
@@ -4,41 +4,80 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../DB/database.dart';
 import '../Models/models.dart';
 
+//USERSTATES:
+final loggedIn = StateProvider<bool>((ref) {
+  var loggedUser = ref.watch(userState);
+  if (loggedUser != null) {
+    return true;
+  }
+  return false;
+});
+final userState = StateProvider<UserModel?>((ref) => null);
+final userErrorState = StateProvider<String?>((ref) => null);
+
 final userRepository =
-    Provider.autoDispose<UserRepo>((ref) => UserRepoImp());
+    Provider.autoDispose<UserRepo>((ref) => UserRepoImp(ref.read));
 
 abstract class UserRepo {
-  Future<UserModel?> getCurrentUser();
-  Future<UserModel> createNewUser(UsersCompanion newUser);
-  Future<bool> updateUser(UsersCompanion user);
+  Future<void> getCurrentUser();
+  Future<void> createNewUser(UsersCompanion newUser);
+  Future<void> updateUser(UsersCompanion user);
 }
 
 class UserRepoImp implements UserRepo {
-  //final Reader _read;
-  UserRepoImp() {
+  final Reader _read;
+  UserRepoImp(this._read) {
     _db = AppDB.getInstance();
+    initState();
   }
 
   late AppDB _db;
 
+  void initState() {
+   getCurrentUser();
+    //checkLoggedIn();
+  }
+
+  void dispose() {
+    //TODO: dispose
+  }
+
+  Future<void> checkLoggedIn() async {
+    final loggedUser = _read(userState);
+    if (loggedUser != null) {
+      _read(loggedIn.notifier).state = true;
+    }
+  }
+
   @override
-  Future<UserModel?> getCurrentUser() async {
+  Future<void> getCurrentUser() async {
     final dbUser = await _db.userDao.checkForUser();
     if (dbUser != null) {
-      return UserModel.fromJson(dbUser.toJson());
+      _read(userState.notifier).state = UserModel.fromJson(dbUser.toJson());
     }
-    return null;
   }
 
   @override
-  Future<UserModel> createNewUser(UsersCompanion newUser) async {
+  Future<void> createNewUser(UsersCompanion newUser) async {
     final dbUser = await _db.userDao.createUser(newUser);
-    return UserModel.fromJson(dbUser.toJson());
+    _read(userState.notifier).state = UserModel.fromJson(dbUser.toJson());
   }
 
   @override
-  Future<bool> updateUser(UsersCompanion user) async {
+  Future<void> updateUser(UsersCompanion user) async {
     final update = await _db.userDao.updateUser(user);
-    return update;
+    //TODO: Handle errors better
+    if (!update) {
+      _read(userErrorState.notifier).state = "User Failed To Update";
+    }
+  }
+}
+
+extension MockUserData on UserRepoImp {
+  Future<void> createNewUserWithMock() async {
+    final mockUser = UsersCompanion(
+        name: const Value("DevonMock"), dateCreated: Value(DateTime.now()));
+
+    createNewUser(mockUser);
   }
 }
